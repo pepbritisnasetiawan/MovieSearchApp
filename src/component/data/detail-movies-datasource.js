7a9a83d0b369fa84accf545f35bf0a11062b8ae5
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-01-30 07:14:52 +0700

    optimize code on detail movie

--- src/component/data/detail-movies-datasource.js
+++ src/component/data/detail-movies-datasource.js
@@ -14,13 +14,4 @@ class DetailMoviesDataSource {
   }
 }
 
-/* class DetailMoviesDataSource {
-  static async getMoviesDetail(tmdbid) {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/${tmdbid}?api_key=68f30be1474675a5b667a150350d0f5c`,
-    );
-    return response.data;
-  }
-} */
-
 export default DetailMoviesDataSource;
