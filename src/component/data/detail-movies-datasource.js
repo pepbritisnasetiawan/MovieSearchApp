0f5d6b866aae273f577fee1428d0dcdadadddf8b
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-01-30 07:14:16 +0700

    optimize code on detail movie

--- src/component/data/detail-movies-datasource.js
+++ src/component/data/detail-movies-datasource.js
@@ -1,12 +1,26 @@
 import axios from 'axios';
 
+const movieClient = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  params: {
+    api_key: '68f30be1474675a5b667a150350d0f5c',
+  },
+});
+
 class DetailMoviesDataSource {
+  static async getMoviesDetail(tmdbid) {
+    const response = await movieClient.get(`movie/${tmdbid}`);
+    return response.data;
+  }
+}
+
+/* class DetailMoviesDataSource {
   static async getMoviesDetail(tmdbid) {
     const response = await axios.get(
       `https://api.themoviedb.org/3/movie/${tmdbid}?api_key=68f30be1474675a5b667a150350d0f5c`,
     );
     return response.data;
   }
-}
+} */
 
 export default DetailMoviesDataSource;
--- webpack.config.js
+++ webpack.config.js
@@ -52,12 +52,7 @@ module.exports = {
     ],
   },
   optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers
-      // (i.e. `terser-webpack-plugin`), uncomment the next line
-      '...',
-      new CssMinimizerPlugin(),
-    ],
+    minimizer: ['...', new CssMinimizerPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({
